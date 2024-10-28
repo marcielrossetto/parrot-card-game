89389a7587acd30eb986860a1f788dac7da26d49
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2024-10-28 00:23:01 -0300

    add funcoes

--- script.js
+++ script.js
@@ -46,3 +46,76 @@ function embaralharCartas() {
   }
 }
 
+// Função para criar as cartas
+
+function criarCartas() {
+  const cartas = document.querySelector(".container");
+  let contadorImagem = 0;
+  cartas.innerHTML = "";
+
+  embaralharCartas();
+
+  listImagensEmbaralhadaDuplicadas = [
+    ...listaImagensEmbaralhada,
+    ...listaImagensEmbaralhada,
+  ].sort(function () {
+    return 0.5 - Math.random();
+  });
+
+  for (let i = 0; i < quantidadeCartas; i++) {
+    contadorImagem = listImagensEmbaralhadaDuplicadas[i];
+    cartas.innerHTML += `
+          <div class="card" onclick="flip(this)" data-parrot="${contadorImagem}">
+            <div class="face front" style="background-image: url('img/${contadorImagem}.gif')"></div>
+            <div class="face back"></div>
+          </div>`;
+  }
+}
+
+// Funções auxiliares
+
+let primeiraCarta = "";
+let segundaCarta = "";
+let primeiroPersonagem = "";
+let segundoPersonagem = "";
+
+function flip(item) {  
+  if (primeiraCarta === "") {    
+      primeiraCarta = item;    
+      item.classList.add("flip");    
+      primeiroPersonagem = item.getAttribute("data-parrot");  
+  } else if (segundaCarta === "") {    
+      segundaCarta = item;    
+      item.classList.add("flip");    
+      segundoPersonagem = item.getAttribute("data-parrot");    
+      setTimeout(verificarSeSaoIguais, 1000);  
+  }  
+  contador++; 
+  setTimeout(encerrarJogo, 500);
+}
+
+function verificarSeSaoIguais() {  
+  if (primeiroPersonagem == segundoPersonagem) {    
+      primeiraCarta = "";    
+      segundaCarta = "";  
+  } else {    
+      primeiraCarta.classList.remove("flip");    
+      segundaCarta.classList.remove("flip");    
+      primeiraCarta = "";    
+      segundaCarta = "";  
+  }
+}
+
+
+function encerrarJogo() {
+  const flipedCards = document.querySelectorAll(".flip");
+  const numeroCartas = document.querySelectorAll(".card");
+  if (flipedCards.length === numeroCartas.length) {
+    const jogadasDivididas = contador;
+    alert(
+      `Você venceu o jogo em ${jogadasDivididas} tentativas e ${contar} segundos, parabéns!`
+    );
+    contar = 0;
+  }
+}
+
